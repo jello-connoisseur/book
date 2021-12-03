f2ae1d03e4bb9eb4a1ea7f77aafbaae9d335c4da
Jewon Hwang <jewon.hwang@gmail.com>
2021-12-03 11:14:36 -0500

    displaying books by accessing the array

--- script.js
+++ script.js
@@ -7,27 +7,49 @@ function Book(name, author, pages, read) {
     this.read = read;
 }
 
+myLibrary.push(new Book ("Bel Ami", "Guy de Maupassant", 394, true));
+myLibrary.push(new Book ("The Masterpiece", "Emile Zola", 400, true));
+myLibrary.push(new Book ("Nana", "Emile Zola", 387, false));
 
 function addBookToLibrary(){
-    const inputName = prompt("Name of the Book");
-    const inputAuthor = prompt("Name of the Author");
-    const inputPages = prompt("Page Count");
-    const didRead = prompt("Read");
-    myLibrary.push(new Book(inputName, inputAuthor, inputPages, didRead));
-    console.table(myLibrary);
+    const newBookBtn = document.querySelector('button');
+
+    newBookBtn.addEventListener('click', () => {
+        const inputName = prompt("Name of the Book");
+        const inputAuthor = prompt("Name of the Author");
+        const inputPages = prompt("Page Count");
+        const didRead = prompt("Read");
+        myLibrary.push(new Book(inputName, inputAuthor, inputPages, didRead));
+    });
+
 }
 addBookToLibrary();
 
+
+
+const container = document.querySelector('#container');
+
 function displayLibrary(){
-    for (let i = 0; i < myLibrary.length; i++){
-        const container = document.querySelector('#container');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = myLibrary[i];
-        container.appendChild(content);
-    }
+    myLibrary.forEach((item, index) => {
+        const bookNumber = document.createElement('div');
+        //add a class to bookNumber
+        bookNumber.classList.add('bookNumber');
+        //add content to bookNumber
+        bookNumber.textContent = 'Book ' + (index+1);
+        //add style to bookNumber
+        bookNumber.setAttribute ('style', 'border: 2px solid; background: yellow; padding: 5px; margin: 5px');
+        //append it to parent
+        container.appendChild(bookNumber);
+        for (var key in item){
+            const bookDisplay = document.createElement('div');
+            bookDisplay.classList.add('bookDisplay');
+            bookDisplay.textContent = `${key}: ${item[key]}`;
+            bookNumber.appendChild(bookDisplay);
+        }
+
+    });    
 }
 
-displayLibrary();
+
 
 
